92d8443cfa975fbdb56db0116baeb4b8c42e56b3
mdgdeveloper <se.rgiomadrigal@gmail.com>
2024-09-07 22:11:56 +0200

    Updated API with proper authOptions to work with GraphAPI requests

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,14 +3,19 @@ import { AdapterUser } from 'next-auth/adapters';
 import AzureADProvider from 'next-auth/providers/azure-ad';
 
 
-const options = {
+export const authOptions = {
   providers: [
     AzureADProvider({
       clientId: process.env.AZURE_CLIENT_ID || '',
       clientSecret: process.env.AZURE_CLIENT_SECRET || '',
       tenantId: process.env.AZURE_TENANT_ID || '',
-      authorization: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/oauth2/v2.0/authorize`,
-      token: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/oauth2/v2.0/token`,
+      // authorization: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/oauth2/v2.0/authorize`,
+      // token: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/oauth2/v2.0/token`,
+      authorization: {
+        params: {
+          scope: "openid profile email offline_access User.Read Group.Read.All GroupMember.Read.All",
+        }
+      }
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
@@ -28,14 +33,21 @@ const options = {
     },
     async jwt({ token, account }: { token: any, account: any }) {
       if (account) {
-        token.accessToken = account.accessToken;
+        token.accessToken = account.access_token;
+        token.accessTokenExpires = account.expires_at * 1000;
+        token.refreshToken = account.refresh_token;
       }
+
+      if (Date.now() < token.accessTokenExpires) {
+        return token;
+      }
+
       return token;
     },
   },
 }
 
-const handler = NextAuth(options);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
 
 
